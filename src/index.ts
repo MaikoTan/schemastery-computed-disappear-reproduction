144ef5d67b6cece7c8dfeb1218c025d6820bb51d
Maiko Tan <maiko.tan.coding@gmail.com>
2024-03-25 10:57:00 +0800

    add repro plugin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { Context, Schema } from 'koishi'
+import { Computed, Context, Schema } from 'koishi'
 
 export const name = 'repro'
 
-export interface Config {}
+export interface Config {
+  testNormal: 'one' | 'two'
+  testComputed: Computed<'one' | 'two'>
+  testComputedUnionConst: Computed<'one' | 'two'>
+  testComputedRadio: Computed<'one' | 'two'>
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  testNormal: Schema.union(['one', 'two']).default('one'),
+  testComputed: Schema.computed(Schema.union(['one', 'two'])).default('one'),
+  testComputedUnionConst: Schema.computed(Schema.union([Schema.const('one'), Schema.const('two')])).default('one'),
+  testComputedRadio: Schema.computed(Schema.union(['one', 'two'])).role('radio').default('one'),
+})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  const logger = ctx.logger('repro')
+  ctx.on('ready', () => {
+    logger.info('plugin loaded')
+  })
 }
